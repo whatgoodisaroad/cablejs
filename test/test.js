f4577c556f594c1a8987aefea6bf5fea8952f8a1
Wyatt Allen <wyatt.allen0@gmail.com>
2014-04-08 23:09:03 -0700

    Added many tests

--- test/test.js
+++ test/test.js
@@ -4,21 +4,146 @@ var
   Cable = require("../dist/cable.min.js");
 
 describe("Define", function() {
-  it("creates event nodes with function syntax", function() {
-    Cable.define({
-      event1:function(event) { }
+  describe("Event", function() {
+    it("creates event nodes with function syntax", function() {
+      Cable.define({
+        event1:function(event) { }
+      });
+
+      Cable._debug().event1.type.should.eql("event");
     });
 
-    Cable._debug().event1.type.should.eql("event");
+    it("creates event nodes with object syntax", function() {
+      Cable.define({
+        event2:{ type:"event", defaultValue:123, wireup:function(event) { } }
+      });
+
+      Cable._debug().event2.type.should.eql("event");
+      Cable._debug().event2.value.should.be.exactly(123);
+    });
+
+    it("allows setting default value without triggering", function() {
+      Cable.define({
+        event3:function(event) { event.setDefault(200); }
+      });
+
+      Cable._debug().event3.value.should.be.exactly(200);
+      Cable._debug().event3.invoked.should.eql(false);
+    });
   });
 
-  it("creates event nodes with object syntax", function() {
-    Cable.define({
-      event2:{ type:"event", defaultValue:123, wireup:function(event) { } }
+  describe("Synthetic", function() {
+    it("creates synthetic node with result syntax", function() {
+      Cable.define({
+        synthetic1:function(event1, _event2, result) { }
+      });
+
+      Cable._debug().synthetic1.type.should.eql("synthetic");
+      Cable._debug().synthetic1["in"].should.eql(["event1", "event2"]);
+      Cable._debug().synthetic1.resultIndex.should.be.exactly(2);
+      Cable._debug().synthetic1.coalesce.should.eql(true);
     });
 
-    Cable._debug().event2.type.should.eql("event");
-    Cable._debug().event2.value.should.be.exactly(123);
+    it("creates synthetic node with respond syntax", function() {
+      Cable.define({
+        synthetic2:function(synthetic1, respond) { }
+      });
+
+      Cable._debug().synthetic2.type.should.eql("synthetic");
+      Cable._debug().synthetic2["in"].should.eql(["synthetic1"]);
+      Cable._debug().synthetic2.resultIndex.should.be.exactly(1);
+      Cable._debug().synthetic2.coalesce.should.eql(false);
+    });
+  });
+
+  describe("Effect", function() {
+    it("creates effect nodes", function() {
+      Cable.define({
+        effect1:function(event1, _event2) { }
+      });
+
+      Cable._debug().effect1.type.should.eql("effect");
+      Cable._debug().effect1["in"].should.eql(["event1", "event2"]);
+    });
+  });
+
+  describe("Data", function() {
+    it("creates data nodes", function() {
+      Cable.define({
+        data1:{ type:"data", value:100 }
+      });
+
+      Cable._debug().data1.type.should.eql("data");
+      Cable._debug().data1.value.should.be.exactly(100);
+    });
+  });
+
+  describe("Library", function() {
+    it("creates library node for AMD module", function() {
+      Cable.define({
+        underscore:{
+          type:"library",
+          path:"underscore.js"
+        }
+      });
+
+      Cable._debug().underscore.type.should.eql("library");
+    });
+
+    it("creates library node for shimmed module", function() {
+      Cable.define({
+        cableLib:{
+          type:"library",
+          path:"../dist/cable.min.js",
+          shim:"Cable"
+        }
+      });
+
+      Cable._debug().cableLib.type.should.eql("library");
+      Cable._debug().cableLib.shim.should.eql("Cable");
+    });
+  });
+
+  describe("Scoped", function() {
+    it("creates scoped nodes", function() {
+      Cable.define({
+        scope1:{
+          x:function(event) { },
+          y:function(x, result) { }
+        },
+        z:function(scope1_y) { }
+      });
+
+      Cable._debug().scope1_x.type.should.eql("event");
+      Cable._debug().scope1_x.out.should.eql(["scope1_y"]);
+
+      Cable._debug().scope1_y.type.should.eql("synthetic");
+      Cable._debug().scope1_y["in"].should.eql(["x"]);
+
+      Cable._debug().z.type.should.eql("effect");
+      Cable._debug().z["in"].should.eql(["scope1_y"]);
+    });
+  });
+
+  describe("Aliases", function() {
+    it("creates alias nodes", function() {
+      Cable.define({
+        color:{ type:"data", value:"#ff0" },
+        colour:{ type:"alias", reference:"color" }
+      });
+
+      Cable._debug().colour.type.should.eql("alias");
+      Cable._debug().colour.reference.should.eql("color");
+    });
+
+    it("follows alias names", function() {
+      Cable.define({
+        testColour:function(forceColourTest, colour) {
+          colour().should.eql("#ff0");
+        },
+        forceColourTest:function(event) { event(new Date()); }
+      });
+    });
   });
 });
 
