e251da8a7d43d9d0f5c8e36c83c85a0fe05cb913
Wyatt Allen <wyatt.allen0@gmail.com>
2014-04-12 18:19:59 -0700

    Expanded 2048 example

--- examples/2048/2048.js
+++ examples/2048/2048.js
@@ -6,28 +6,56 @@ define(function() {
   var game = {
     clone:function(stage) {
       return {
+        score:stage.score,
+        step:stage.step,
         rows:stage.rows,
         columns:stage.columns,
         spawns:stage.spawns.slice(0),
-        grid:stage.grid.slice(0),
+        winningTile:stage.winningTile,
+        grid:stage.grid.map(function(r) { return r.slice(0); }),
         last:{
-          created:stage.last.created,
-          moved:stage.last.moved,
-          merged:stage.last.merged
+          created:stage.last.created.map(function(c) {
+            return { row:c.row, column:c.column, value:c.value, step:c.step };
+          }),
+          moved:stage.last.moved.map(function(c) {
+            return {
+              row1:c.row1,
+              column1:c.column1,
+              row2:c.row2,
+              column2:c.column2,
+              value:c.value,
+              step:c.step 
+            };
+          }),
+          merged:stage.last.merged.map(function(c) {
+            return {
+              row1:c.row1,
+              column1:c.column1,
+              row2:c.row2,
+              column2:c.column2,
+              value:c.value,
+              step:c.step 
+            };
+          }),
+          hasMoved:stage.last.hasMoved
         }
       };
     },
 
     blank:function() {
       var stage = {
+        score:0,
+        step:0,
         rows:4,
         columns:4,
         spawns:[ 2, 4 ],
+        winningTile:2048,
         grid:[],
         last:{
           created:[],
           moved:[],
-          merged:[]
+          merged:[],
+          hasMoved:false
         }
       };
 
@@ -48,7 +76,8 @@ define(function() {
       stage.last = {
         created:[],
         moved:[],
-        merged:[]
+        merged:[],
+        hasMoved:false
       };
 
       return stage;
@@ -96,7 +125,12 @@ define(function() {
 
       stage.grid[row][col] = rand(stage.spawns);
 
-      stage.last.created.push({ row:row, column:col });
+      stage.last.created.push({
+        row:row,
+        column:col,
+        value:stage.grid[row][col],
+        step:stage.step
+      });
 
       return stage;
     },
@@ -108,7 +142,9 @@ define(function() {
     move:function(stage, direction) {
       stage = this.resetLast(this.clone(stage));
 
-      var row, col, hasMoved = false;
+      ++stage.step;
+
+      var row, col;
 
       function trans(row, col) {
         if (direction === "south") {
@@ -142,28 +178,54 @@ define(function() {
           }
 
           var destRow = row;
-          while ( destRow < stage.rows - 1 && 
-                 (get(destRow + 1, col) === 0 ||
-                  get(destRow + 1, col) === get(row, col))) {
+          while (
+            // Reached bottom:
+            destRow < stage.rows - 1 && 
+
+            // Either a movement or a merge:
+            (
+              // Blank below ==> move:
+              (get(destRow + 1, col) === 0) 
+
+              ||
+
+              // Same number below, and not merged in this turn ==> merge:
+              (
+                get(destRow + 1, col) === get(row, col) &&
+                !mergeTable[(destRow + 1) + "x" + col]
+              )
+            )
+          ) {
             ++destRow;
+
+            if (get(destRow, col) === get(row, col)) {
+              break;
+            }
           }
 
           if (destRow !== row) {
-            hasMoved = true;
+            stage.last.hasMoved = true;
 
             var 
               t1 = trans(row, col),
               t2 = trans(destRow, col);
 
-            if ( get(destRow, col) === get(row, col)) {
+            if (get(destRow, col) === get(row, col) &&
+                !mergeTable[destRow + "x" + col]) {
               set(destRow, col, 2 * get(destRow, col));
               set(row, col, 0);
+
+              stage.score += get(destRow, col);
+
+              mergeTable[destRow + "x" + col] = true;
 
               stage.last.merged.push({
                 row1:   t1.row,
                 column1:t1.column,
                 row2:   t2.row,
-                column2:t2.column
+                column2:t2.column,
+                value:get(destRow, col),
+                step:stage.step
               });
             }
             else {
@@ -174,18 +236,40 @@ define(function() {
                 row1:   t1.row,
                 column1:t1.column,
                 row2:   t2.row,
-                column2:t2.column
-              })
+                column2:t2.column,
+                value:get(destRow, col),
+                step:stage.step
+              });
             }
           }
         }
       }
 
-      if (hasMoved) {
+      if (stage.last.hasMoved) {
         stage = this.spawn(stage, direction);
       }
 
       return stage;
+    },
+
+    isGameOver:function(state) {
+      return (
+        !this.move(state, "north").last.hasMoved &&
+        !this.move(state, "south").last.hasMoved &&
+        !this.move(state, "east").last.hasMoved &&
+        !this.move(state, "west").last.hasMoved
+      );
+    },
+
+    isGameWon:function(stage) {
+      for (var row = 0; row < stage.rows; ++row) {
+        for (var col = 0; col < stage.columns; ++col) {
+          if (stage.grid[row][col] === stage.winningTile) {
+            return true;
+          }
+        }
+      }
+      return false;
     }
   };
 
